f392b80f44edae0feef1020b2640bff6330c4c98
allencode11 <al.el.en.lina@gmail.com>
2021-09-23 17:02:22 +0300

    adding gitignore and applicationDbContext

---
+++ TechnicalTest.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace TechnicalTest.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+               
+        }
+
+    }
+}
--- TechnicalTest.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TechnicalTest.Models
-{
-    public class Class1
-    {
-    }
-}
