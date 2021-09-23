a64c20787db33449c5d09a391caebcb432c8909d
allencode11 <al.el.en.lina@gmail.com>
2021-09-23 16:39:37 +0300

    adding the template for ASP.NET core web api and creating the structure of the project

---
+++ TechnicalTest.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TechnicalTest.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ TechnicalTest.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TechnicalTest.Models
+{
+    public class Class1
+    {
+    }
+}
